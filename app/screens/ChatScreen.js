72d95947a8e46560e72ed30270da421a6c9f68ce
sunn-e <sunnydhoke22@gmail.com>
2020-12-26 15:18:02 +0530

    Update ChatScreen.js

--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -18,16 +18,84 @@ const styles = StyleSheet.create({
   },
 });
 */
-import * as React from "react";
-import { Button, View } from "react-native";
-export default function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate("Login")}
+import React from "react";
+import {
+  Button,
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  SafeAreaView,
+} from "react-native";
+
+import { TextInput } from "react-native-gesture-handler";
+
+import { GiftedChat } from "react-native-gifted-chat";
+import FireDbaaS from "../config/FireDbaaS";
+
+export default class ChatScreen extends React.Component {
+  state = {
+    messages: [],
+  };
+
+  get user() {
+    return {
+      _id: FireDbaaS.uid,
+      //name: this.props.navigation.state.params.name,
+      name: this.state.name,
+    };
+  }
+
+  componentDidMount() {
+    FireDbaaS.get((message) =>
+      this.setState((previous) => ({
+        messages: GiftedChat.append(previous.messages, message),
+      }))
+    );
+  }
+  componentWillUnmount() {
+    FireDbaaS.off();
+  }
+  render() {
+    const chat = (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={FireDbaaS.send}
+        user={this.user}
       />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
+    );
+    if (Platform.OS === "android") {
+      return (
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior="padding"
+          keyboardVerticalOffset={25}
+          enabled
+        >
+          {chat}
+        </KeyboardAvoidingView>
+      );
+    }
+    return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F4F5F7",
+  },
+  circle: {
+    width: 500,
+    height: 500,
+    borderRadius: 500 / 2,
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    left: -120,
+    right: -20,
+  },
+});
