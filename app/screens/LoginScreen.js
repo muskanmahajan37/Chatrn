5ec3ecc1784632db8be2ecb47b1bf3927b84fa79
sunn-e <sunnydhoke22@gmail.com>
2020-12-26 14:38:30 +0530

    Added Login Screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -52,12 +52,71 @@ const styles = StyleSheet.create({
 */
 
 import * as React from "react";
-import { Button, View } from "react-native";
+import { Button, View, StyleSheet, Image, Text } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
-export default function LoginScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Go to Chat" onPress={() => navigation.navigate("Chat")} />
-    </View>
-  );
+import { Ionicons } from "@expo/vector-icons";
+
+export default class LoginScreen extends React.Component {
+  state = {
+    name: "",
+  };
+  continue = () => {
+    this.props.navigation.navigate("Chat", { name: this.state.name });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="your username"
+            onChangeText={(name) => {
+              this.setState({ name });
+            }}
+            value={this.state.name}
+          ></TextInput>
+        </View>
+
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <TouchableOpacity onPress={this.continue}>
+            <Ionicons
+              name="md-arrow-round-forward"
+              size={30}
+              color="#000"
+            ></Ionicons>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F5F7",
+  },
+  circle: {
+    width: 500,
+    height: 500,
+    borderRadius: 250,
+    backgroundColor: "#FF4800",
+    position: "absolute",
+    left: -120,
+    right: -20,
+  },
+  input: {
+    borderStyle: "solid",
+    marginTop: 30,
+    borderRadius: 30,
+    paddingHorizontal: 30,
+    borderColor: "black",
+  },
+});
